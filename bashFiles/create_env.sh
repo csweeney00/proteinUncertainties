4eacd0ce5aad4d37adfa7a2e3e8219ee691f37a7
Charlotte Sweeney <32967382+csweeney00@users.noreply.github.com>
2024-05-31 17:42:48 +0000

    idk

--- bashFiles/create_env.sh
+++ bashFiles/create_env.sh
@@ -10,6 +10,7 @@ eval "yes | conda create -n enn-env python=3.11"
 eval "conda activate enn-env"
 
 echo "Installing packages"
+eval "yes | conda install pip"
 eval "yes | conda install pandas"
 eval "yes | conda install matplotlib"
 eval "yes | conda install jupyter"
