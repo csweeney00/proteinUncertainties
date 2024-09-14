18ce035d4fb482dbe36d1180d14eae11eb21b8fb
Charlotte Sweeney <32967382+csweeney00@users.noreply.github.com>
2024-09-14 02:53:47 +0100

    Create build_loops_dataset.py

---
+++ training_code/build_loops_dataset.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+
+def build_loops_dataset():
+    onlyfiles = [f for f in listdir('./pdbs') if isfile(join('./pdbs', f))]
+    codes = set([f.split("_")[0] for f in onlyfiles])
+
+    # Limiting codes to sequences of length 5 as this the most populous sequence length
+    # 5 is far too big. Length 11 has a manageable number of conformations for us to handle (arount 18000)
+    codes = [code for code in codes if len(code) == 11]
+
+    data = []
+    print("Producing data")
+    for code in codes:
+        conformations = list(filter(lambda x: x.startswith(code), onlyfiles))
+        conformation_indxs = [conf.split("_")[1][:-4] for conf in conformations]
+        data.append([code, conformation_indxs])
+
+    df = pd.DataFrame(data=data, columns = ['aa_seq', 'conformation_indxs'])
+    print("Saving the dataframe")
+    df.to_pickle('./loops_dataset_len_11.pkl')
+    return
+
+if __name__ == "__main__":
+    build_loops_dataset()
